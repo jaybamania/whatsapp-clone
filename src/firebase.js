ed5d313573a75a47f7a04fe0d3db36c260939ffe
jaybamania <jaykumardiu@gmail.com>
2021-04-23 16:42:12 +0530

    react-with-firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZLzA6C9Bv0f4CmoM_CG2qpy3igE-QSLw",
+    authDomain: "whats-app-clone-df7c4.firebaseapp.com",
+    projectId: "whats-app-clone-df7c4",
+    storageBucket: "whats-app-clone-df7c4.appspot.com",
+    messagingSenderId: "765195307538",
+    appId: "1:765195307538:web:8c7b61c234a0824a2d66cb",
+    measurementId: "G-HBHV5XY2QP"
+  };
