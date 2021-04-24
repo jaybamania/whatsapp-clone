b72aa0c57cc56876d5a44096b5f24e42d208c729
jaybamania <jaykumardiu@gmail.com>
2021-04-24 13:54:20 +0530

    Competed-UI-and-Connected-with-firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyAZLzA6C9Bv0f4CmoM_CG2qpy3igE-QSLw",
     authDomain: "whats-app-clone-df7c4.firebaseapp.com",
@@ -7,3 +9,12 @@ const firebaseConfig = {
     appId: "1:765195307538:web:8c7b61c234a0824a2d66cb",
     measurementId: "G-HBHV5XY2QP"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+  const db = firebaseApp.firestore()
+  const auth = firebase.auth()
+  const provider = new firebase.auth.GoogleAuthProvider()
+
+  export {auth, provider}
+
+export default db
